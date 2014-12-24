99946d86e75682f329f8a8291224987b38d142bd
aMahdavian <amin@polonious.com.au>
2014-12-24 20:21:10 +1100

    fix a typo in footer

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,7 @@
 }
 if(typeof define === "function" && define.amd) {
 	define(_crossfilter);
-}else if (typeof module === "object" && module.exports)
+}else if (typeof module === "object" && module.exports){
 	module.exports = _crossfilter;
 }else{
 	window.crossfilter=_crossfilter;
